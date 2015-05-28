c61094e59582439b5fff291b2df922507b97b674
rix1 <rikardeide@gmail.com>
2015-05-28 12:56:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Evolutionary Computation - Assignment 1
+
+
+This README should include:
+
+- Whose EA you compared fitness results with? Be sure to include their name and student number as well as the average, maximum, and minimum fitness scores of their EA.
+- The results of statistical tests that compare the fitness results of your EA with a classmate’s EA. You should clearly state if there is a statistically significant difference between the average, maximum and minimum fitness of your EA versus your classmate’s EA.
+- A working theory as to why there is (or is not) a statistically significant difference between the fitness results of the two EAs (200 words maximum).
---
+++ src/org/rix1/Chromosome.java
@@ -0,0 +1,113 @@
+package org.rix1;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class Chromosome {
+
+    /**
+     * The list of cities, which are the genes of this chromosome.
+     */
+    protected int[] cityList;
+
+    /**
+     * The cost of following the cityList order of this chromosome.
+     */
+    protected double cost;
+
+    /**
+     * @param cities The order that this chromosome would visit the cities.
+     */
+    Chromosome(City[] cities) {
+        Random generator = new Random();
+        cityList = new int[cities.length];
+        //cities are visited based on the order of an integer representation [o,n] of each of the n cities.
+        for (int x = 0; x < cities.length; x++) {
+            cityList[x] = x;
+
+        }
+
+        //shuffle the order so we have a random initial order
+        for (int y = 0; y < cityList.length; y++) {
+            int temp = cityList[y];
+            int randomNum = generator.nextInt(cityList.length);
+            cityList[y] = cityList[randomNum];
+            cityList[randomNum] = temp;
+        }
+
+        calculateCost(cities);
+    }
+
+    /**
+     * Calculate the cost of the specified list of cities.
+     *
+     * @param cities A list of cities.
+     */
+    void calculateCost(City[] cities) {
+        cost = 0;
+        for (int i = 0; i < cityList.length - 1; i++) {
+            double dist = cities[cityList[i]].proximity(cities[cityList[i + 1]]);
+            cost += dist;
+        }
+    }
+
+    /**
+     * Get the cost for this chromosome. This is the amount of distance that
+     * must be traveled.
+     */
+    double getCost() {
+        return cost;
+    }
+
+    /**
+     * @param i The city you want.
+     * @return The ith city.
+     */
+    int getCity(int i) {
+        return cityList[i];
+    }
+
+    /**
+     * Set the order of cities that this chromosome would visit.
+     *
+     * @param list A list of cities.
+     */
+    void setCities(int[] list) {
+        for (int i = 0; i < cityList.length; i++) {
+            cityList[i] = list[i];
+        }
+    }
+
+    /**
+     * Set the index'th city in the city list.
+     *
+     * @param index The city index to change
+     * @param value The city number to place into the index.
+     */
+    void setCity(int index, int value) {
+        cityList[index] = value;
+    }
+
+    /**
+     * Sort the chromosomes by their cost.
+     *
+     * @param chromosomes An array of chromosomes to sort.
+     * @param num         How much of the chromosome list to sort.
+     */
+    public static void sortChromosomes(Chromosome chromosomes[], int num) {
+        Chromosome ctemp;
+        boolean swapped = true;
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < num - 1; i++) {
+                if (chromosomes[i].getCost() > chromosomes[i + 1].getCost()) {
+                    ctemp = chromosomes[i];
+                    chromosomes[i] = chromosomes[i + 1];
+                    chromosomes[i + 1] = ctemp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+}
